2a4e477bbf18017b72041ba6fa359033d1c38437
Michele Focchi <michele.focchi@unitn.it>
2025-11-16 12:33:47 +0100

    cleanup of the header

--- include/ros_impedance_controller/controller.h
+++ include/ros_impedance_controller/controller.h
@@ -1,28 +1,25 @@
 #ifndef IMP_CONTROLLER_H
 #define IMP_CONTROLLER_H
 
-// Ros
+// ROS
 #include <ros/ros.h>
 #include <sensor_msgs/JointState.h>
+
+// custom messages & services
 #include <ros_impedance_controller/EffortPid.h>
-#include <ros_impedance_controller/get_effort_pid.h>
-#include <ros_impedance_controller/pid.h>
 #include <ros_impedance_controller/set_pids.h>
-// PluginLib
+#include <ros_impedance_controller/pid.h>
+
+// pluginlib
 #include <pluginlib/class_list_macros.hpp>
-// Ros control
-#include <controller_interface/controller.h>
+
+// ros_control
 #include <controller_interface/multi_interface_controller.h>
-// Hardware interfaces
 #include <hardware_interface/joint_command_interface.h>
 
-//pid
-#include <control_toolbox/pid.h>
 // Eigen
 #include <Eigen/Dense>
-//map
-#include <tf/transform_broadcaster.h>
-  
+
 namespace ros_impedance_controller
 {
 
@@ -36,51 +33,51 @@ public:
     ~Controller();
 
     /**
-         * @brief Initializes sample controller
-         * @param hardware_interface::RobotHW* robot hardware interface
-         * @param ros::NodeHandle& Root node handle
-         * @param ros::NodeHandle& Supervisor node handle
-         */
-    bool init(hardware_interface::RobotHW* robot_hw, ros::NodeHandle& root_nh, ros::NodeHandle& controller_nh);
+     * @brief Initializes sample controller
+     * @param hardware_interface::RobotHW* robot hardware interface
+     * @param ros::NodeHandle& Root node handle
+     * @param ros::NodeHandle& Supervisor node handle
+     */
+    bool init(hardware_interface::RobotHW* robot_hw,
+              ros::NodeHandle& root_nh,
+              ros::NodeHandle& controller_nh) override;
 
     /**
-         * @brief Starts the sample controller when controller manager request it
-         * @param const ros::Time& time Time
-         */
-    void starting(const ros::Time& time);
+     * @brief Starts the sample controller when controller manager request it
+     * @param const ros::Time& time Time
+     */
+    void starting(const ros::Time& time) override;
 
     /**
-         * @brief Updates the sample controller according to the control
-         * frequency (task frequency)
-         * @param const ros::Time& time Time
-         * @param const ros::Duration& Period
-         */
-    void update(const ros::Time& time, const ros::Duration& period);
+     * @brief Updates the sample controller according to the control
+     * frequency (task frequency)
+     * @param const ros::Time& time Time
+     * @param const ros::Duration& Period
+     */
+    void update(const ros::Time& time, const ros::Duration& period) override;
 
     /**
-         * @brief Stops the sample controller when controller manager request it
-         * @param const ros::time& Time
-         */
-    void stopping(const ros::Time& time);
+     * @brief Stops the sample controller when controller manager request it
+     * @param const ros::time& Time
+     */
+    void stopping(const ros::Time& time) override;
 
 private:
-
     void commandCallback(const sensor_msgs::JointState &msg);
     bool setPidsCallback(set_pids::Request& req,
                          set_pids::Response& res);
-    void updateDiscreteGains(const std::vector<double> & joint_p_gain_, const std::vector<double> & joint_d_gain_,  const std::vector<double> & joint_i_gain_, const int & N, const double & Ts);
-     
 
-    ros::Subscriber command_sub_;
+    // ROS interfaces
+    ros::Subscriber  command_sub_;
     ros::ServiceServer set_pids_srv_;
-    ros::Publisher effort_pid_pub;
-    
-    /** @brief Number of joints */
-    unsigned int num_joints_;
+    ros::Publisher   effort_pid_pub;
+    ros::NodeHandle* root_nh_;
+
     /** @brief Joint names */
     std::vector<std::string> joint_names_;
     /** @brief Joint states for reading positions, velocities and efforts and writing effort commands */
     std::vector<hardware_interface::JointHandle> joint_states_;
+
     /** @brief Desired joint efforts */
     Eigen::VectorXd des_joint_efforts_;
     /** @brief Desired joint positions */
@@ -95,21 +92,22 @@ private:
     Eigen::VectorXd integral_action_;
     /** @brief Old joint error */
     Eigen::VectorXd derivative_action_;
+
     /** @brief Actual P value for the joints PID controller */
     std::vector<double> joint_p_gain_;
     /** @brief Actual I value for the joints PID controller */
     std::vector<double> joint_i_gain_;
     /** @brief Actual D value for the joints PID controller */
     std::vector<double> joint_d_gain_;
+
     std::vector<std::string> joint_type_;
     Eigen::VectorXd measured_joint_position_;
     Eigen::VectorXd measured_joint_velocity_;
-
-     Eigen::VectorXd joint_positions_old_;
+    Eigen::VectorXd joint_positions_old_;
 
     /** @brief Desired joint efforts computed by the PIDs */
     Eigen::VectorXd des_joint_efforts_pids_;
-    
+
     /** @brief Discrete implem varables */
     Eigen::VectorXd error_;
     Eigen::VectorXd error1_;
@@ -118,19 +116,15 @@ private:
     const int N = 8;
     double Ts = 0.;
 
-    std::vector<std::vector<double> > force_;
-    std::vector<std::vector<double> > torque_;
-    std::vector<std::vector<double> > normal_;
-
-    ros::NodeHandle * root_nh_;
     bool verbose = false;
     bool discrete_implementation_ = false;
-
 };
 
-
-PLUGINLIB_EXPORT_CLASS(ros_impedance_controller::Controller, controller_interface::ControllerBase);
+PLUGINLIB_EXPORT_CLASS(ros_impedance_controller::Controller,
+                       controller_interface::ControllerBase);
 
 } //@namespace ros_impedance_controller
 
-#endif //IMP_CONTROLLER_H
+#endif // IMP_CONTROLLER_H
+
+
